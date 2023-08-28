6cb624a7684dd66573122a07e435035fcb1dc75f
Mike Misiura <mikemisiura@gmail.com>
2023-08-28 15:01:13 -0600

    changed to mysql

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,11 +6,13 @@ const dbName = 'petDB';
 const username = 'root';
 const password = 'Password1!';
 
-const sequelize = new Sequelize(dbName, username, password, {
-    host: 'localhost',
-    port: 3306,
-    dialect: 'mysql'
-});
+// const sequelize = new Sequelize(dbName, username, password, {
+//     host: 'localhost',
+//     port: 3306,
+//     dialect: 'sqlite'
+// });
+
+const sequelize = new Sequelize('sqlite::memory:')
 
 PetFactory(sequelize);
 
